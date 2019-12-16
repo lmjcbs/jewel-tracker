33bd239fae1014189d7d7f1221661a62126ecf6a
lmjcbs <hello@liamjacobs.dev>
2019-12-16 00:57:11 +0000

    add login/signup routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,29 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  get "/signup" do
+    erb :signup
+  end
+
+  post "/login" do
+    @user = User.find_by(username: params[:username], password: params[:password])
+    
+    if @user.nil?
+      erb :error
+    else
+      session[:user_id] = @user.id
+      redirect '/jewels'
+    end
+  end
+
+  post "/signup" do
+    @user = User.new(username: params["username"], password: params["password"])
+
+    if @user.save #requires password field when signing up
+      redirect "/login"
+    else
+      redirect "/error"
+    end
+  end
+
 end
