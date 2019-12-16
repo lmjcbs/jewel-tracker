ba4a016c2409a4302890251726ae2e7134acd53c
lmjcbs <hello@liamjacobs.dev>
2019-12-16 15:16:04 +0000

    moved helper methods to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < Sinatra::Base
   post "/login" do
     user = User.find_by(username: params[:username])
 
-    if !user.nil? && user.authenticate(params[:password])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect "/jewels"
     else
@@ -33,12 +33,38 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     user = User.new(username: params[:username], password: params[:password])
 
-    if user.save #requires password field when signing up
+    if user.save #returns true/false. requires password field when signing up
       session[:user_id] = user.id
       redirect "/jewels"
     else
       redirect "/error"
     end
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/"
+  end
+
+  get "/jewels" do
+    @session = session
+    if logged_in?(session)
+      erb :jewels
+    else
+      erb :error
+    end
+  end
+
+  helpers do
+    
+    def logged_in?(session_hash)
+      !!session_hash[:user_id]
+    end
+
+    def self.current_user(session_hash)
+      User.find_by(id: session_hash[:user_id])
+    end
+
   end
 
 end
