7b037b2f059a5acf0b262a08156cd380c34518ee
lmjcbs <hello@liamjacobs.dev>
2019-12-16 01:15:36 +0000

    fixed user variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < Sinatra::Base
     user = User.find_by(username: params[:username])
 
     if !user.nil? && user.authenticate(params[:password])
-      session[:user_id] = @user.id
+      session[:user_id] = user.id
       redirect "/jewels"
     else
       redirect "/error"
@@ -31,9 +31,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    @user = User.new(username: params["username"], password: params["password"])
+    user = User.new(username: params["username"], password: params["password"])
 
-    if @user.save #requires password field when signing up
+    if user.save #requires password field when signing up
       redirect "/login"
     else
       redirect "/error"
