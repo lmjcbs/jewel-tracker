8d6a612b2e19de8d3fce75f52758d171dd301760
lmjcbs <hello@liamjacobs.dev>
2019-12-17 20:59:07 +0000

    enable sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "mysecret"
   end
 
   get "/" do
@@ -21,7 +23,6 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     user = User.find_by(username: params[:username])
-
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect "/jewels"
@@ -32,7 +33,6 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     user = User.new(username: params[:username], password: params[:password])
-
     if user.save #returns true/false. requires password field when signing up
       session[:user_id] = user.id
       redirect "/jewels"
@@ -44,27 +44,6 @@ class ApplicationController < Sinatra::Base
   get "/logout" do
     session.clear
     redirect "/"
-  end
-
-  get "/jewels" do
-    @session = session
-    if logged_in?(session)
-      erb :jewels
-    else
-      erb :error
-    end
-  end
-
-  helpers do
-    
-    def logged_in?(session_hash)
-      !!session_hash[:user_id]
-    end
-
-    def self.current_user(session_hash)
-      User.find_by(id: session_hash[:user_id])
-    end
-
   end
 
 end
