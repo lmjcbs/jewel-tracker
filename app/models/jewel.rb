736f01d0a275a55fc7fb38605a87b50c9edeea6b
lmjcbs <hello@liamjacobs.dev>
2019-12-19 00:42:17 +0000

    add validations

--- app/models/jewel.rb
+++ app/models/jewel.rb
@@ -2,4 +2,7 @@ class Jewel < ActiveRecord::Base
 
   belongs_to :user
 
+  validates :name, :colour, :location_found, presence: true
+  validates :weight, :value, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
 end
